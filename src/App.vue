a8f3dd4662a468260a0fdf6b22a32fcd7c11afc8
Maria Kotliarova <itwassoclear@gmail.com>
2020-06-11 12:01:17 +0300

    developed basic functionality

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <navbar />
+    <div class="container">
+      <router-view />
+    </div>
+  </div>
+</template>
+
+<script>
+import Navbar from '@/components/Navbar';
+export default {
+  components: {
+    'navbar' : Navbar,
+  }
+}
+</script>
+
+<style lang="scss">
+  @import "~materialize-css/dist/css/materialize.min.css";
+</style>
