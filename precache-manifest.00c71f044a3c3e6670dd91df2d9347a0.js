27a084c2798572015add0530fc953448fee3ccac
Maria Kotliarova <itwassoclear@gmail.com>
2020-07-20 20:12:43 +0300

    deploy

---
+++ precache-manifest.00c71f044a3c3e6670dd91df2d9347a0.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db40956bfb055ce1f728",
+    "url": "/to-do-app/css/app.eb2a6e33.css"
+  },
+  {
+    "revision": "72d1db777ded5ff7fea0",
+    "url": "/to-do-app/css/chunk-0b90f5ea.561c3dfd.css"
+  },
+  {
+    "revision": "68db7aacbc55605c9aa3",
+    "url": "/to-do-app/css/chunk-vendors.3e4956bb.css"
+  },
+  {
+    "revision": "6345a5044c542da1936b19b31ec6dce6",
+    "url": "/to-do-app/index.html"
+  },
+  {
+    "revision": "db40956bfb055ce1f728",
+    "url": "/to-do-app/js/app.f39e2dfa.js"
+  },
+  {
+    "revision": "90292e7cfe622290e638",
+    "url": "/to-do-app/js/chunk-062578a1.64ac1fc1.js"
+  },
+  {
+    "revision": "72d1db777ded5ff7fea0",
+    "url": "/to-do-app/js/chunk-0b90f5ea.16d1d948.js"
+  },
+  {
+    "revision": "6088ef40634932630aa8",
+    "url": "/to-do-app/js/chunk-410f89f6.fd420d7d.js"
+  },
+  {
+    "revision": "68db7aacbc55605c9aa3",
+    "url": "/to-do-app/js/chunk-vendors.2738fa03.js"
+  },
+  {
+    "revision": "b73a4d8e21551abd7cf34027145c362e",
+    "url": "/to-do-app/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/to-do-app/robots.txt"
+  }
+]);
